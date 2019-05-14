25aae997ada2340fa1319145d021e438dd0ea160
freestyler@734@gmail.com <freestyler734@gmail.ru>
2019-05-14 23:57:09 +0300

    5

--- src/com/company/_5/ScheduingAndBinarySearchTrees/BinarySearchTree.java
+++ src/com/company/_5/ScheduingAndBinarySearchTrees/BinarySearchTree.java
@@ -1,7 +1,6 @@
 package com.company._5.ScheduingAndBinarySearchTrees;
 
 
-import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -9,20 +8,17 @@ import java.util.stream.IntStream;
 public class BinarySearchTree<K extends Comparable<K>, V> {
 
     private Node<K, V> root;
-    private int size = 0;
-    private int maxDepth = 0;
+    private int height = 0;
 
     BinarySearchTree() { }
 
     public void add(K key, V value) {
         Node<K, V> node = new Node<>(key, value);
         add(node);
-        size++;
     }
 
-
     private void add(Node<K, V> node) {
-        int curDepth = 1;
+        int curHeight = 1;
         if (root == null) {
             root = node;
         } else {
@@ -50,95 +46,28 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
                     normalizeRank(current);
                     return;
                 }
-                curDepth++;
+                curHeight++;
             }
         }
-        maxDepth = maxDepth > curDepth ? maxDepth : curDepth;
+        height = height > curHeight ? height : curHeight;
     }
-
-    public void print() {
-        Queue<Node<K, V>> queue = new LinkedList<>();
-        queue.add(root);
-        levelRecursivePassing(queue);
-        System.out.println("Max depth: " + getMaxDepth());
-    }
-
-    private void symmetricPrint(Node<K, V> node) {
-        if (node == null) {
-            return;
-        }
-
-        symmetricPrint(node.leftChild);
-        System.out.println(node);
-        symmetricPrint(node.rightChild);
-    }
-
-    private void straightPrint(Node<K, V> node) {
-        if (node == null) {
-            return;
-        }
-
-        System.out.println(node);
-        symmetricPrint(node.leftChild);
-        symmetricPrint(node.rightChild);
-    }
-
-    public void levelPassing() {
-        // System.out.println(node);
-        Queue<Node<K, V>> queue = new LinkedList<>();
-
-        queue.add(root);
-
-        while (queue.size() != 0) {
-            Node current = queue.poll();
-
-            if (current.leftChild != null) {
-                queue.add(current.leftChild);
-            }
-            if (current.rightChild != null) {
-                queue.add(current.rightChild);
-            }
-        }
-    }
-
-    private void levelRecursivePassing(Queue<Node<K,V>> queue) {
-        if (queue.size() == 0) {
-            return;
-        }
-
-        Node current = queue.poll();
-        System.out.println(current+ ", ");
-
-        if (current.leftChild != null) {
-            queue.add(current.leftChild);
-        }
-        if (current.rightChild != null) {
-            queue.add(current.rightChild);
-        }
-        levelRecursivePassing(queue);
-    }
-
-    public int getMaxDepth() {
-        return maxDepth;
-    }
-
 
     public void visualizeTree(){
 
-        Node<K,V>[] heapStructure = new Node[(int) Math.pow(2, maxDepth) - 1];
+        Node<K,V>[] heapStructure = new Node[(int) Math.pow(2, height) - 1];
 
         straightPassing(root, heapStructure, 0);
 
         for (int i = 0; i < heapStructure.length; i++) {
             int curLayer = (int) (Math.log(i + 1)/Math.log(2)) + 1;
-            IntStream.range(0, (int) (Math.pow(2, maxDepth - curLayer)) - 1).forEach((e) -> System.out.print("    "));
+            IntStream.range(0, (int) (Math.pow(2, height - curLayer)) - 1).forEach((e) -> System.out.print("    "));
             System.out.printf(" %s ", heapStructure[i] == null ? "  " : heapStructure[i].key);
-            IntStream.range(0, (int) (Math.pow(2, maxDepth - curLayer))).forEach((e) -> System.out.print("    "));
+            IntStream.range(0, (int) (Math.pow(2, height - curLayer))).forEach((e) -> System.out.print("    "));
             if (i == 0 || i + 1 == (Math.pow(2, curLayer) - 1)) {
-                IntStream.range(0, maxDepth - curLayer).forEach(e -> System.out.print("\n"));
+                IntStream.range(0, height - curLayer).forEach(e -> System.out.print("\n"));
             }
         }
-        System.out.println("\n" + IntStream.range(0, (int) Math.pow(2, maxDepth) + 1).mapToObj((e) -> "____").collect(Collectors.joining()) + "\n");
+        System.out.println("\n" + IntStream.range(0, (int) Math.pow(2, height) + 1).mapToObj((e) -> "____").collect(Collectors.joining()) + "\n");
 
     }
 
@@ -152,93 +81,9 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
         straightPassing(node.rightChild,heapStructure, 2 * index + 2);
     }
 
-    public static class Node<K extends Comparable<K>, V> {
-        private Node parent;
-        private Node leftChild;
-        private Node rightChild;
-        private int rank = 1;
-        private K key;
-        private V value;
-
-        public Node(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public Node<K, V> findMin() {
-            return this.leftChild == null ? this : this.leftChild.findMin();
-        }
-
-        public Node<K, V> findMax() {
-            return this.rightChild == null ? this : this.rightChild.findMax();
-        }
-
-        public Node<K, V> find(K key) {
-            if (this.key.compareTo(key) == 0) {
-                return this;
-            } else if (key.compareTo(this.key) < 0) {
-                Node<K, V> left = this.leftChild;
-                return left == null ? null : left.find(key);
-            } else {
-                Node<K, V> right = this.rightChild;
-                return right == null ? null : right.find(key);
-            }
-        }
-
-        public int getLevel() {
-            Node<K, V> current = this;
-            int counter = 1;
-            while (current.parent != null) {
-                counter++;
-                current = current.parent;
-            }
-
-            return counter;
-        }
-
-        public int getMaxDepth() {
-            int left = leftChild == null ? 0 : leftChild.getMaxDepth();
-            int right = rightChild == null ? 0 : rightChild.getMaxDepth();
-
-            return Math.max(left, right) + 1;
-        }
-
-        public Node<K, V> getNextLarger() {
-            if (this.rightChild != null) {
-                return this.rightChild.findMin();
-            }
-            Node<K, V> current = this;
-            while (current.parent != null &&
-                   current.parent.rightChild != null &&
-                   current.key.compareTo((K) current.parent.rightChild.key) == 0) {
-                current = current.parent;
-            }
-            return current.parent;
-        }
-
-        @Override
-        public String toString() {
-            return "{" + key + ", " + value + '}';
-        }
-    }
-
-    private Node<K, V> getMinNode(Node<K, V> node) {
-        if (node.leftChild == null) {
-            return node;
-        }
-        return getMinNode(node.leftChild);
-    }
-
-    private Node<K, V> getMaxNode(Node<K, V> node) {
-        if (node.rightChild == null) {
-            return node;
-        }
-        return getMaxNode(node.rightChild);
-    }
-
     public V delete(K key) {
         Node<K, V> deleted = deleteNode(root, false, key);
-        maxDepth = (root == null ? 0 : root.getMaxDepth());
+        height = (root == null ? 0 : getHeight(root));
         return deleted == null ? null : deleted.value;
     }
 
@@ -272,7 +117,7 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
             }
         } else if (node.leftChild != null && node.rightChild != null) {
 
-            Node<K, V> current = node.getNextLarger();
+            Node<K, V> current = node.getSuccessor();
             Node<K, V> curParent = current.parent;
             if (curParent.equals(node)) {
                 deleteRightReleation(curParent, current);
@@ -311,28 +156,54 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
 
     }
 
+    /**
+     * Устанавливает отношение между узлами parent и leftChild,
+     * как родитель и левый потомок
+     * @param parent
+     * @param leftChild
+     */
     private void setLeftRelation(Node parent, Node leftChild) {
         if (parent != null) parent.leftChild = leftChild;
         if (leftChild != null) leftChild.parent = parent;
 
     }
 
+    /**
+     * Устанавливает отношение между узлами parent и rightChild,
+     * как родитель и правый потомок
+     * @param parent
+     * @param rightChild
+     */
     private void setRightRelation(Node parent, Node rightChild) {
         if (parent != null) parent.rightChild = rightChild;
         if (rightChild != null) rightChild.parent = parent;
 
     }
 
+    /**
+     * Обнуляет ссылки узлов parent и leftChild, для удаления отношения родитель и левый потомок
+     * @param parent
+     * @param leftChild
+     */
     private void deleteLeftReleation(Node parent, Node leftChild) {
         if (parent != null) parent.leftChild = null;
         if (leftChild != null) leftChild.parent = null;
     }
 
+    /**
+     * Обнуляет ссылки узлов parent и rightChild, для удаления отношения родитель и правый потомок
+     * @param parent
+     * @param rightChild
+     */
     private void deleteRightReleation(Node parent, Node rightChild) {
         if (parent != null) parent.rightChild = null;
         if (rightChild != null) rightChild.parent = null;
     }
 
+    /**
+     * Исправляет значения rank в узле после удаления
+     * @param node
+     */
     private void normalizeRank(Node node) {
         while (node != null) {
             int sum = 1;
@@ -344,34 +215,117 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
 
     }
 
-    public V find(K key) {
-        Node<K, V> found = root.find(key);
-        return found == null ? null: found.value;
+    /**
+     * Определяет высоту поддерева узла node
+     * @return int
+     */
+    public int getHeight(Node node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return Math.max(getHeight(node.leftChild), getHeight(node.rightChild)) + 1;
     }
 
-    public V findMin() {
-        return root == null ? null : root.findMin().value;
-    }
+    /**
+     * Реализация узла для бинарного дерева поиска.
+     * Узел содержит:
+     *  1) parent - ссылка на родительский узел
+     *  2) leftChild - ссылка на левый дочерний узел
+     *  3) rightChild - ссылка на правый дочерний узел
+     *  4) rank - количество элементов в поддереве текущего узла
+     *  5) key - ключ узла, значение для соблюдения инвариант поддерева
+     *  6) value - хранимое значение узла
+     *
+     * @param <K> extends Comparable<K> - обобщенный тип для key
+     * @param <V> - тип для value
+     */
+    public static class Node<K extends Comparable<K>, V> {
+        private Node parent;
+        private Node leftChild;
+        private Node rightChild;
+        private int rank = 1;
+        private int height = 1;
+        private K key;
+        private V value;
 
-    public V findMax() {
-        return root == null ? null : root.findMax().value;
-    }
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
 
-    public void testNextLarger() {
+        /**
+         * Находит узел с минимальным ключом в поддереве текущего узла
+         * @return Node<K, V> минимальный узел или null
+         */
+        public Node<K, V> findMin() {
+            return this.leftChild == null ? this : this.leftChild.findMin();
+        }
 
-        Queue<Node<K, V>> queue = new LinkedList<>();
+        /**
+         * Находит узел с максимальным ключом в поддереве текущего узла
+         * @return Node<K, V> максимальный узел или null
+         */
+        public Node<K, V> findMax() {
+            return this.rightChild == null ? this : this.rightChild.findMax();
+        }
 
-        queue.add(root);
-
-        while (queue.size() != 0) {
-            Node current = queue.poll();
-            System.out.printf("node: %s, next larger: %s \n", current, current.getNextLarger());
-            if (current.leftChild != null) {
-                queue.add(current.leftChild);
+        /**
+         * Находит узел с указанным ключом key в поддереве текущего узла
+         * @param key - ключ для поиска
+         * @return Node<K, V> узел с ключом key или null
+         */
+        public Node<K, V> find(K key) {
+            if (this.key.compareTo(key) == 0) {
+                return this;
+            } else if (key.compareTo(this.key) < 0) {
+                Node<K, V> left = this.leftChild;
+                return left == null ? null : left.find(key);
+            } else {
+                Node<K, V> right = this.rightChild;
+                return right == null ? null : right.find(key);
             }
-            if (current.rightChild != null) {
-                queue.add(current.rightChild);
+        }
+
+        /**
+         * Возвращает узел с меньшим ключом среди узлов с ключом больше текущего
+         * (следущий больший)
+         * @return Node<K, V> узел или null
+         */
+        public Node<K, V> getSuccessor() {
+            if (this.rightChild != null) {
+                return this.rightChild.findMin();
             }
+            Node<K, V> current = this;
+            while (current.parent != null &&
+                    current.parent.rightChild != null &&
+                    current.key.compareTo((K) current.parent.rightChild.key) == 0) {
+                current = current.parent;
+            }
+            return current.parent;
+        }
+
+        /**
+         * Возвращает узел с большим ключом среди узлов с ключом меньше текущего
+         * (следущий меньший)
+         * @return Node<K, V> узел или null
+         */
+        public Node<K, V> getPredecessor() {
+            if (this.leftChild != null) {
+                return this.leftChild.findMin();
+            }
+            Node<K, V> current = this;
+            while (current.parent != null &&
+                    current.parent.leftChild != null &&
+                    current.key.compareTo((K) current.parent.leftChild.key) == 0) {
+                current = current.parent;
+            }
+            return current.parent;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + ", " + value + '}';
         }
     }
 }
